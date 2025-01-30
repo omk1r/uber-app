1da3a979351a912f8408b0e279ff1d5839f12375
Omkar <jadhavpatil2999@gmail.com>
2025-01-30 23:20:19 +0530

    user authentication added

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+  console.log(token);
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
