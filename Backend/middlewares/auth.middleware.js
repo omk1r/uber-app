e4b58300b2cf6ccc6466d3fce1aa9c31514abd84
Omkar <jadhavpatil2999@gmail.com>
2025-01-31 14:38:42 +0530

    logout user done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,12 +1,17 @@
 const userModel = require('../models/user.model');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(' ')[1];
-  console.log(token);
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const isBlackListed = await blacklistTokenModel.findOne({ token: token });
+  if (isBlackListed) {
+    return res.status(401).json({ message: 'Unauthorized access' });
   }
 
   try {
