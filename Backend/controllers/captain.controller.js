3b22d7e32be8be1b1f6a4e6533940636ac5cf66a
Omkar <jadhavpatil2999@gmail.com>
2025-02-03 14:18:04 +0530

    captain profile and logout route added

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
 const { validationResult } = require('express-validator');
@@ -66,4 +67,16 @@ module.exports.loginCaptain = async (req, res, next) => {
 
   res.cookie('token', token);
   res.status(200).json({ token, captain });
+};
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+  res.status(200).json({ captain: req.captain });
+};
+
+module.exports.logoutCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+  await blacklistTokenModel.create({ token });
+
+  res.clearCookie('token');
+  res.status(200).json({ message: 'Logout successfully' });
 };
