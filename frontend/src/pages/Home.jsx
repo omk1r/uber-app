8cf208d8914190eacf596435dd47482abbf16469
Omkar <jadhavpatil2999@gmail.com>
2025-02-10 23:27:35 +0530

    home ui started

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="relative">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt=""
+        className="top-5 left-5 absolute w-20"
+      />
+    </div>
+  );
 };
 
 export default Home;
