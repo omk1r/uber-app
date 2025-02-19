ca1d2ff5bb0d1eded3d468ef4c3ee4fe5f33aeb7
Omkar <jadhavpatil2999@gmail.com>
2025-02-19 18:47:34 +0530

    create ride completed

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const rideController = require('../controllers/ride.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post(
   '/create',
-  body('userId')
-    .isString()
-    .isLength({ min: 24, max: 24 })
-    .withMessage('Invalid user id'),
+  authMiddleware.authUser,
   body('pickup')
     .isString()
     .isLength({ min: 3 })
@@ -15,7 +14,12 @@ router.post(
   body('destination')
     .isString()
     .isLength({ min: 3 })
-    .withMessage('Invalid destination address')
+    .withMessage('Invalid destination address'),
+  body('vehicleType')
+    .isString()
+    .isIn(['auto', 'car', 'moto'])
+    .withMessage('Invalid vehicle types'),
+  rideController.createRide
 );
 
 module.exports = router;
